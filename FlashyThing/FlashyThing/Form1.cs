d99fb54e1effccb7fc65ca09c107e6893b1d6d03
LiBin <anddicli@gmail.com>
2013-05-26 16:48:37 +0800

    loop statements regarding color changing for Chapter-2

---
+++ FlashyThing/FlashyThing/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FlashyThing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (Visible)
+            {
+                for (int i = 0; i < 254 && Visible == true; i++)
+                {
+                    this.BackColor = Color.FromArgb(i, 255 - i, i);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(5);
+                }
+                for (int i = 255; i > 0 && Visible == true; i--)
+                {
+                    this.BackColor = Color.FromArgb(i, 255 - i, i);
+                    Application.DoEvents();
+                    System.Threading.Thread.Sleep(5);
+                }
+            }
+            
+        }
+    }
+}
