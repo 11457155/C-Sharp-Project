a01caddbb18e00f930b8dcd2abb582d5b8af777e
LiBin <anddicli@gmail.com>
2013-06-02 17:55:53 +0800

    the pool puzzle exercise regarding loop and if statements

---
+++ PoolPuzzle/PoolPuzzle/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace PoolPuzzle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            String Poem = GetPuzzle();
+
+            MessageBox.Show(Poem);
+           
+        }
+
+        
+        private string GetPuzzle()
+        {
+            int x = 0;
+            String Poem = "";
+
+            while (x < 4)
+            {
+                Poem = Poem + "a";
+                if (x < 1)
+                {
+                    Poem = Poem + " ";
+                }
+                Poem = Poem + "n";
+                if (x > 1)
+	            {
+                    Poem = Poem + " oyster";
+                    x = x + 2;
+
+	            }
+
+                if (x == 1)
+                {
+                    Poem = Poem + "noys ";
+                }
+
+                if (x < 1)
+	            {
+                    Poem = Poem + "oise ";
+	            }
+                
+                x = x + 1;
+            }
+            return Poem;
+        }
+        
+    }
+}
