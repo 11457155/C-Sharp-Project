79282814bafc53e650e74428e539218668b9fdd0
LiBin <anddicli@gmail.com>
2013-06-02 20:57:30 +0800

    new class to pop up content input from screen

---
+++ 3_Talk/3_Talk/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _3_Talk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size = Talk.ActualTalk(textBox1.Text, (int)numericUpDown1.Value);
+            MessageBox.Show("The message length of this is: " + size);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 3_Talk/3_Talk/Talk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace _3_Talk
+{
+    class Talk
+    {
+        public static int ActualTalk(string wordsToSay, int times)
+        {
+            string finalWords = "";
+            for (int i = 0; i < times; i++)
+            {
+                finalWords += wordsToSay + "\n";
+            }
+            MessageBox.Show(finalWords);
+            return finalWords.Length;
+        }
+    }
+}
