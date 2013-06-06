158c6c0f77b6ade79c49c0bcdbaf5ea3931c9751
LiBin <anddicli@gmail.com>
2013-06-06 22:04:54 +0800

    elephant: reference usage

---
+++ Elephant/Elephant/Elephant.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Elephant
+{
+    class Elephant
+    {
+        public int earSize;
+        public string name;
+
+        public string whoAmI()
+        {
+            return " my name is: " + name + " my earsize is: " + earSize;
+        }
+    }
+}
---
+++ Elephant/Elephant/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Elephant
+{
+    public partial class Form1 : Form
+    {
+        Elephant lioyd;
+        Elephant lucinda;
+        Elephant temp;
+        public Form1()
+        {
+            InitializeComponent();
+
+            lioyd = new Elephant(){ earSize = 20, name = "lioyd"};
+            lucinda = new Elephant() { earSize = 30, name = "lucinda"};
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(lioyd.whoAmI());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(lucinda.whoAmI());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            temp = lioyd;
+            lioyd = lucinda;
+            MessageBox.Show("Objects swapped");
+            lucinda = temp;
+        }
+    }
+}
