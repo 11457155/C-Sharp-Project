6bdf95159135051ee922b2c9d6858e35352845f8
LiBin <anddicli@gmail.com>
2013-09-07 17:14:57 +0800

    code of chapter 4-typing games

---
+++ TypingGames/TypingGames/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TypingGames
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        Stats stats = new Stats();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add((Keys)random.Next(65, 90));
+
+            if (listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Game Over!");
+                timer1.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+             /* if the user presses a key down, if this key is in the listbox, then remove it from the
+             listbox, and update the statusStrip value accordingly, and then make this game a little bit
+             faster */
+
+            if(listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+                
+                /* code to make this game a little bit faster */
+                if (timer1.Interval > 400)
+                {
+                    timer1.Interval -= 20;
+                }
+                else if (timer1.Interval > 250)
+                {
+                    timer1.Interval -= 10;
+                }
+                else if (timer1.Interval > 100)
+                {
+                    timer1.Interval -= 5;
+                }
+
+                DifficultyProgressBar.Value = 800 - timer1.Interval;
+
+                stats.update(true);
+
+            }
+            else
+            {
+                stats.update(false);
+            }
+
+            /* code used to update the StatusStrip value */
+            CorrectLabel.Text = "Correct: " + stats.correct;
+            MissedLabel.Text = "Missed: " + stats.missed;
+            TotalLabel.Text = "Total: " + stats.total;
+            AccuracyLabel.Text = "Accuracy: " + stats.accuracy;
+        }
+
+        private void listBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /* if the user presses a key down, if this key is in the listbox, then remove it from the
+             listbox, and update the statusStrip value accordingly, and then make this game a little bit
+             faster */
+
+            if (listBox1.Items.Contains(e.KeyCode))
+            {
+                listBox1.Items.Remove(e.KeyCode);
+                listBox1.Refresh();
+
+                /* code to make this game a little bit faster */
+                if (timer1.Interval > 400)
+                {
+                    timer1.Interval -= 10;
+                }
+                else if (timer1.Interval > 250)
+                {
+                    timer1.Interval -= 5;
+                }
+                else if (timer1.Interval > 100)
+                {
+                    timer1.Interval -= 2;
+                }
+                else if (timer1.Interval > 2)
+                {
+                    timer1.Interval -= 1;
+                }
+
+               DifficultyProgressBar.Value = 800 - timer1.Interval;
+
+                stats.update(true);
+
+            }
+            else
+            {
+                stats.update(false);
+            }
+
+            /* code used to update the StatusStrip value */
+            CorrectLabel.Text = "Correct: " + stats.correct;
+            MissedLabel.Text = "Missed: " + stats.missed;
+            TotalLabel.Text = "Total: " + stats.total;
+            AccuracyLabel.Text = "Accuracy: " + stats.accuracy;
+        }
+    }
+}
---
+++ TypingGames/TypingGames/Stats.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TypingGames
+{
+    class Stats
+    {
+        public int correct = 0;
+        public int missed = 0;
+        public int total = 0;
+        public int accuracy = 0;
+
+        public void update(bool correctKey)
+        {
+            /* no matter if this key is right or wrong, total number shall always increase */
+            total++;
+            if (correctKey == true)
+            {
+                correct++;
+            }
+            else
+            {
+                missed++;
+            }
+
+            accuracy = correct * 100 / (correct + missed);
+        }
+    }
+}
